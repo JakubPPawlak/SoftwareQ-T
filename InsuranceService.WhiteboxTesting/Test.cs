507c70717f4b77c62c98d0b97cecbe137a6cf0b5
Jakub Pawlak <s00172641@mail.itsligo.ie>
2019-03-19 09:39:18 +0000

    Add project files.

---
+++ InsuranceService.WhiteboxTesting/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Assignment;
+
+namespace InsuranceService.WhiteboxTesting
+{
+    [TestFixture]
+    class Test
+    {
+        public Assignment.InsuranceService SUT;
+
+        [SetUp]
+        public void initialize()
+        {
+            SUT = new Assignment.InsuranceService();
+        }
+
+        [Test]
+        public void test1()
+        {
+
+        }
+    }
+}
---
+++ Whitebox/InsuranceService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class InsuranceService 
+    {
+        public float CalcPremium(int age, string gender)
+        {
+            float premium;
+
+            if(gender == "female")
+            {
+                if(age >= 18 && age <= 30)
+                {
+                    premium = 5.0F;
+                }
+                else
+                {
+                    if(age >= 31)
+                    {
+                        premium = 2.5F;
+                    }
+                    else
+                    {
+                        premium = 0.0F;
+                    }
+                }
+            }
+            else if(gender == "male")
+            {
+                if(age >= 18 && age <= 35)
+                {
+                    premium = 6.0F;
+                }
+                else
+                {
+                    if(age >= 36)
+                    {
+                        premium = 5.0F;
+                    }
+                    else
+                    {
+                        premium = 0.0F;
+                    }
+                }
+            }
+            else
+            {
+                premium = 0.0F;
+            }
+
+            if(age >= 50)
+            {
+                premium = premium * 0.15F;
+            }
+
+            return premium;
+        }
+    }
+}
