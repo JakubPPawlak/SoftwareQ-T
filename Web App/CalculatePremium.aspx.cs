468546beb67e5ca681befeb50371dee11ebffc58
Jakub Pawlak <s00172641@mail.itsligo.ie>
2019-03-19 12:24:16 +0000

    Added Web App and Simple form with fields Calculations work

---
+++ Web App/CalculatePremium.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_App
+{
+    public partial class CalculatePremium : System.Web.UI.Page
+    {
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected float Calculate(int age, string gender)
+        {
+            Assignment.InsuranceService SUT = new Assignment.InsuranceService();
+            float result;
+
+            result = SUT.CalcPremium(age, gender);
+            return result;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            float result;
+            int age = int.Parse(TextBox1.Text);
+            string gender = TextBox2.Text;
+            result = Calculate(age, gender);
+            TextBox3.Text = result.ToString();
+        }
+    }
+}
