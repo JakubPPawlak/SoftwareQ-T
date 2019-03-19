dd5dfdcff5f0e7216d8374c9c3910c36ab283966
Jakub Pawlak <s00172641@mail.itsligo.ie>
2019-03-19 11:33:19 +0000

    Added white box tests (10)

--- InsuranceService.WhiteboxTesting/Test.cs
+++ InsuranceService.WhiteboxTesting/Test.cs
@@ -20,9 +20,73 @@ namespace InsuranceService.WhiteboxTesting
         }
 
         [Test]
-        public void test1()
+        public void Test_01()
         {
+            float result = SUT.CalcPremium(18, "female");
+            Assert.That(result, Is.EqualTo(5));
+        }
 
+        [Test]
+        public void Test_02()
+        {
+            float result = SUT.CalcPremium(50, "female");
+            Assert.That(result, Is.EqualTo(2.875));
+        }
+
+        [Test]
+        public void Test_03()
+        {
+            float result = SUT.CalcPremium(31, "female");
+            Assert.That(result, Is.EqualTo(2.5));
+        }
+
+        [Test]
+        public void Test_04()
+        {
+            float result = SUT.CalcPremium(15, "female");
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Test_05()
+        {
+            float result = SUT.CalcPremium(18, "male");
+            Assert.That(result, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Test_06()
+        {
+            float result = SUT.CalcPremium(50, "male");
+            Assert.That(result, Is.EqualTo(5.75));
+        }
+
+        [Test]
+        public void Test_07()
+        {
+            float result = SUT.CalcPremium(36, "male");
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Test_08()
+        {
+            float result = SUT.CalcPremium(15, "male");
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Test_09()
+        {
+            float result = SUT.CalcPremium(25, "orange");
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Test_10()
+        {
+            float result = SUT.CalcPremium(50, "orange");
+            Assert.That(result, Is.EqualTo(0));
         }
     }
 }
