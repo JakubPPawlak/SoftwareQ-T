770c8d1b7efd5b8816b068cdbc4f8ba1502f1bf2
Gary <s00171995@mail.itsligo.ie>
2019-03-19 11:40:04 +0000

    added blackbox testing

---
+++ BlackboxTests/InsuranceServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fit;
+using fit.Fixtures;
+using Assignment;
+
+namespace BlackboxTests
+{
+    public class InsuranceServiceTests : ColumnFixture
+    {
+        public static InsuranceService insurance;
+        public static int age;
+        public static string gender;
+
+        public float premium()
+        {
+            insurance = new InsuranceService();
+
+            return insurance.CalcPremium(age, gender);
+        }
+    }
+}
+
